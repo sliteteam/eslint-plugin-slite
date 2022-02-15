c37407a9894193b6bd42adf5374ebc045e77d9b5
jasonphillips <jason.surname.phillips@gmail.com>
2022-02-15 12:04:43 -0600

    chore: add rule for unnecessary usecallback

---
+++ tests/lib/rules/no-memoization-for-dom-element-callbacks.jsx
@@ -0,0 +1,100 @@
+const rule = require("../../../lib/rules/no-memoization-for-dom-element-callbacks.js")
+
+const ValidComponentWithoutMemo = `
+  const MyComponent = () => {
+    const handleClick = (event) => {
+      event.preventDefault
+    }
+    return (
+      <div
+        onClick={handleClick}
+      />
+    )
+  }
+`
+
+const ValidComponentWithInlineFunction = `
+  const MyComponent = () => {
+    return (
+      <div
+        onClick={(event) => event.preventDefault}
+      />
+    )
+  }
+`
+
+const ValidComponentWithMixedUses = `
+  const MyComponent = () => {
+    const handleClick = React.useCallback((event) => {
+      event.preventDefault
+    }, [])
+    return (
+      <div>
+        <CustomComponent
+          onClick={handleClick}
+        />
+        <div onClick={handleClick} />
+      </div>
+    )
+  }
+`
+
+const InvalidComponentWithMemo = `
+  const MyComponent = () => {
+    const handleClick = React.useCallback((event) => {
+      event.preventDefault
+    }, [])
+    return (
+      <div
+        onClick={handleClick}
+      />
+    )
+  }
+`
+
+const InvalidComponentWithMemoAlt = `
+  const MyComponent = () => {
+    const handleClick = useCallback((event) => {
+      event.preventDefault
+    }, [])
+    return (
+      <div
+        onClick={handleClick}
+      />
+    )
+  }
+`
+
+// eslint-disable-next-line node/no-unpublished-require
+const { RuleTester } = require("eslint")
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 10, ecmaFeatures: { jsx: true } } })
+
+const errors = [
+  {
+    message: rule.message,
+  },
+]
+
+ruleTester.run("no-memoization-for-dom-element-callbacks", rule, {
+  valid: [
+    {
+      code: ValidComponentWithoutMemo,
+    },
+    {
+      code: ValidComponentWithInlineFunction,
+    },
+    {
+      code: ValidComponentWithMixedUses,
+    }
+  ],
+  invalid: [
+    {
+      code: InvalidComponentWithMemo,
+      errors,
+    },
+    {
+      code: InvalidComponentWithMemoAlt,
+      errors,
+    }
+  ],
+})
